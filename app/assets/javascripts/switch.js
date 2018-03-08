529c18d10791496a73e5096676e665788f4b904a
bg-wa <bill.j.grow@gmail.com>
2018-03-07 21:16:48 -0800

    - Bug Fixes - Screens

--- app/assets/javascripts/switch.js
+++ app/assets/javascripts/switch.js
@@ -3,14 +3,14 @@ var code_editor
 $(document).ready(
     function(){
         $("textarea").each(function() {
-            var editor = CodeMirror.fromTextArea($(this).get(0), {
+            code_editor = CodeMirror.fromTextArea($(this).get(0), {
                 lineNumbers: true,
                 mode: 'text/x-sh',
                 theme: 'monokai'
             });
-            code_editor = editor
         });
-        updatePayloadEditor('/payloads/switch1', 'payload.txt', 1)
+        var switch_position = $('.switch-1').length > 0 ? 1 : 2
+        updatePayloadEditor('/payloads/switch' + switch_position, 'payload.txt', switch_position)
     }
 );
 
@@ -28,8 +28,8 @@ function updatePayloadEditor(path, file, switch_position){
     var file_path = path + '/' + file
     $.getJSON('/studio/payload_script?file=' + file_path, function(response){
         code_editor.setValue(response.payload)
-        $('.edit-file-link').removeClass('active-file')
-        $('.switch-' + switch_position + '.edit-file-link:contains("' + file + '")').addClass('active-file')
+        $('.edit-file-link').removeClass('active')
+        $('.switch-' + switch_position + '.edit-file-link:contains("' + file + '")').addClass('active')
         $('.switch-' + switch_position + '.local-path').text(file_path)
     });
 }
