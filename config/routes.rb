ab282db439a441341fc51208e5f98c8b2f89d9a1
bg-wa <bill.j.grow@gmail.com>
2018-03-08 18:46:20 -0800

    routes cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
-  get 'studio/switch1'
-  get 'studio/switch2'
-  get 'studio/extensions'
-  get 'studio/payloads'
-  get 'studio/loot'
-  get 'studio/debug'
-  get 'studio/git'
-  get 'studio/learn'
+  namespace 'studio' do
+    get 'switch1'
+    get 'switch2'
+    get 'extensions'
+    get 'payloads'
+    get 'loot'
+    get 'debug'
+    get 'git'
+    get 'learn'
 
-  get 'studio/clone_repo'
-  get 'studio/git_command'
-  get 'studio/delete_repo'
-  get 'studio/eject_bunny'
-  get 'studio/copy_payload'
-  get 'studio/sync_extensions'
-  get 'studio/raw_file'
-  get 'studio/payload_script'
+    # API
+    get 'clone_repo'
+    get 'git_command'
+    get 'delete_repo'
+    get 'eject_bunny'
+    get 'copy_payload'
+    get 'sync_extensions'
+    get 'raw_file'
+    get 'payload_script'
 
-  post 'studio/write_payload'
-
+    post 'write_payload'
+  end
+  get 'studio', to: 'studio#switch1'
 
   root 'studio#switch1'
 end
