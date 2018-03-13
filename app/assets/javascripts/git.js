4415181ed1a4300dc010306e38c10caced446171
bg-wa <bill.j.grow@gmail.com>
2018-03-13 09:00:48 -0700

    Create File UI - Needs refresh after create - Needs upload code

--- app/assets/javascripts/git.js
+++ app/assets/javascripts/git.js
@@ -14,11 +14,11 @@ function cloneRepo(){
         $.getJSON('/studio/clone_repo?repo=' + repo, function(response){
             location.reload();
         }, function(err){
-            $('#alert-dialog-content').html("<p class='alert'>Repo NOT Cloned</p>" + close_alert_dialog_button)
+            $('#alert-dialog-content').html("<div class='text-center'>GITHUB</div><p class='alert'>Repo NOT Cloned</p>" + close_alert_dialog_button)
             $('#alert-dialog').foundation('reveal', 'open');
         });
     }else{
-        $('#alert-dialog-content').html("<p class='alert'>Must be a Bash Bunny Repo</p>" + close_alert_dialog_button)
+        $('#alert-dialog-content').html("<div class='text-center'>GITHUB</div><p class='alert'>Must be a Bash Bunny Repo</p>" + close_alert_dialog_button)
         $('#alert-dialog').foundation('reveal', 'open');
     }
 }
@@ -27,7 +27,7 @@ function gitCommand(){
     $.getJSON('/studio/git_command?command=' + $('#git-command').val(), function(response){
         $('#git-command-response').text(response.output);
     }, function(err){
-        $('#alert-dialog-content').html("<p class='alert'>Command NOT Executed</p>" + close_alert_dialog_button)
+        $('#alert-dialog-content').html("<div class='text-center'>GITHUB</div><p class='alert'>Command NOT Executed</p>" + close_alert_dialog_button)
         $('#alert-dialog').foundation('reveal', 'open');
     });
 }
--- app/assets/javascripts/payloads.js
+++ app/assets/javascripts/payloads.js
@@ -10,7 +10,7 @@ $(document).ready(
 );
 
 function copyPayload(switch_position, path){
-    $('#alert-dialog-content').html("<p class=''>Are you sure you want to overwrite Switch " + switch_position +"?</p>" +
+    $('#alert-dialog-content').html("<div class='text-center'>PAYLOAD</div><div>Are you sure you want to overwrite Switch " + switch_position +"?</div>" +
         "<button class='button secondary' onclick='writeToSwitch(" + switch_position + ", \"" + path + "\")'>Yes</button>" +
         "<button class='button' onclick='$(\"#alert-dialog\").foundation(\"reveal\", \"close\");'>Cancel</button>");
     $('#alert-dialog').foundation('reveal', 'open');
@@ -19,11 +19,11 @@ function copyPayload(switch_position, path){
 function writeToSwitch(switch_position, path){
     $.getJSON('/studio/copy_payload?switch_position=' + switch_position + "&path="+ path, function(response){
         if(response.status == 200){
-            $('#alert-dialog-content').html("<p class='success'>Payload copied to Switch " + switch_position +"</p>" +
+            $('#alert-dialog-content').html("<div class='text-center'>PAYLOAD</div><p class='success'>Payload copied to Switch " + switch_position +"</p>" +
                 close_alert_dialog_button)
             $('#alert-dialog').foundation('reveal', 'open');
         }else{
-            $('#alert-dialog-content').html("<p class='alert'>Payload NOT Copied</p>" + close_alert_dialog_button)
+            $('#alert-dialog-content').html("<div class='text-center'>PAYLOAD</div><p class='alert'>Payload NOT Copied</p>" + close_alert_dialog_button)
             $('#alert-dialog').foundation('reveal', 'open');
         }
     });
