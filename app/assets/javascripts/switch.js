c135158d37f61b878b69df982c223fa412c744e9
bg-wa <bill.j.grow@gmail.com>
2018-03-13 10:59:46 -0700

    Update File List after write (untested)

--- app/assets/javascripts/switch.js
+++ app/assets/javascripts/switch.js
@@ -24,6 +24,7 @@ function writePayload(switch_position, file, create) {
             $('#alert-dialog-content').html("<div class='text-center'>PAYLOAD</div><p class='success'>Payload saved to " + response.file + "</p>" +
                 close_alert_dialog_button)
             $('#alert-dialog').foundation('reveal', 'open');
+            updateFileList(response.file)
         } else {
             $('#alert-dialog-content').html("<div class='text-center'>PAYLOAD</div><p class='alert'>File Not Saved</p>" + close_alert_dialog_button)
             $('#alert-dialog').foundation('reveal', 'open');
@@ -57,4 +58,11 @@ function createFileInput(switch_position) {
 
 function createFile(switch_position){
   writePayload(switch_position, '/payloads/switch' + switch_position + '/' + $('#create-file-input').val(), true);
+}
+
+function updateFileList(file_path){
+    var split_file_name = file_path.split('/');
+    var file_name = split_file_name[split_file_name.length -1];
+    var switch_position = split_file_name[split_file_name.length -2].replace('switch', '');
+    $('#file-list').prepend('<li><a class="switch-' + switch_position + ' edit-file-link" onclick="updatePayloadEditor(\'/payloads/switch' + switch_position + '\', \'' + file_name + '\', ' + switch_position + ')">' + file_name + '</a></li>')
 }
